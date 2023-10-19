8fa62aba0a5a40aef2678615bcaf32283ebacfc8
Aleksej Savica <sova11@mail.ru>
2023-10-19 18:12:24 +0300

    123

---
+++ config.py
@@ -0,0 +1,13 @@
+# import requests
+
+# TOKEN = '5755910136:AAE2VgCE3tQLXN2MIX-xC7qpuomlZuB0UyA'
+TOKEN = '5755910136:AAFQ4NoDRdiWZjobwGYHgPD2rzi7Qtb1j2c'
+
+APY_KEY = 'DoJ0YrnhZlsewb1tWLhiOwMKP2LlmTlM'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'тугрик': 'MNT'
+}
